3c4da65d3ff4e60eebce8544ec32f7f732dff09f
Nicolo Veneziale <nicoloveneziale@gmail.com>
2024-10-31 12:50:05 +0000

    Styled the ship placement screen and created the render board component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import "./styles.css";
 import { renderEntryPage, startButton } from "./entryPage.js";
+import renderBoard from "./renderBoard.js";
 
 import Player from "./Player";
 import Ship from "./Ship.js";
 import ComputerPlayer from "./ComputerPlayer.js";
 
+const body = document.querySelector("body");
 renderEntryPage();
 let name = "";
 
 startButton.addEventListener("click", () => {
   name = document.querySelector("#name").value;
-  document.querySelector("body").innerHTML = "";
+  const player = new Player(name);
+  const computer = new ComputerPlayer();
+  body.innerHTML = "";
+  const selectionGameGrid = renderBoard(player.gameBoard);
+  const placeText = document.createElement("p");
+  placeText.innerHTML = "Place your ships " + name + "!";
+  placeText.id = "place-text";
+  body.append(placeText, selectionGameGrid);
+  body.classList.add("flex");
 });
--- src/renderBoard.js
+++ src/renderBoard.js
@@ -1,3 +1,15 @@
-function renderBoard(board) {}
+function renderBoard(gameBoard) {
+  const gameGrid = document.createElement("div");
+  gameGrid.classList.add("game-grid");
+  for (const y in gameBoard.board) {
+    for (const x in gameBoard.board[y]) {
+      const button = document.createElement("button");
+      button.classList.add("game-cell");
+      button.id = x + y;
+      gameGrid.appendChild(button);
+    }
+  }
+  return gameGrid;
+}
 
 export default renderBoard;
